bec454b23ec51b2085d620d4a831432a1ae999a7
PRAISANEE\anutsarus.su <anutsarus.su@thailandpost.com>
2024-02-27 16:20:48 +0700

    try v-for each

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,9 +3,6 @@
     <NavbarMenu></NavbarMenu>
     <div class="grid grid-cols-4 gap-4 py-2">
       <img src="../img/Beagle Puppies.jpg" class="w-20 h-30">
-      <img :src="ImageBeaglePuppy" class="w-20 h-30">
-      <img :src="ImageBeaglePuppy" class="w-20 h-30">
-      <img :src="ImageBeaglePuppy" class="w-20 h-30">
     </div>
   </div>
 </template>
@@ -18,7 +15,8 @@ export default {
   name: 'HomeView',
   data () {
     return {
-      ImageBeaglePuppy: process.env.IMAGE_BEAGLE_PUPPY
+      ImageBeaglePuppy: process.env.IMAGE_BEAGLE_PUPPY,
+      url_image: []
     }
   },
   components: {

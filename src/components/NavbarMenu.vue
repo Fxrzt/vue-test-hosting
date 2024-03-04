e522480c10456670ce095e656c138919f1913084
PRAISANEE\anutsarus.su <anutsarus.su@thailandpost.com>
2024-03-04 16:13:28 +0700

    add navbar responsive

--- src/components/NavbarMenu.vue
+++ src/components/NavbarMenu.vue
@@ -1,40 +1,53 @@
 <template>
-  <nav class="py-5 px-2 bg-black">
-    <div
-      class="max-w-screen flex flex-wrap items-center justify-between pl-12 pr-12"
-    >
-      <a href="#" class="flex items-center space-x-3 rtl:space-x-reverse">
+  <section class="">
+    <nav class=" max-w-screen flex items-center justify-between px-4 md:px-4.5 lg:px-6 bg-black ">
+      <a href="/" class=" space-x-3 rtl:space-x-reverse">
         <span
           class="sty_logo self-center text-2xl font-semibold whitespace-nowrap text-white"
           >Gavyn Miles</span
         >
       </a>
-      <div class="hidden w-full md:block md:w-auto">
-        <ul
-          class=" flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse mt-0 border-0  "
-        >
-          <li v-for="(item, index) in menubar" :key="index">
-            <a
-              v-if="index == 4"
-              @click="fn_getName"
-              href="/"
-              class="topic_text_TH block z-1 py-1.5 px-4 text-white rounded md:text-white hover:text-white hover:bg-transparent"
-              style="border: 2px solid #fff;"
-              aria-current="page"
-              >{{ item.name }}</a
-            >
-            <a
-              v-else
-              @click="fn_getName"
-              :href="item.name_path"
-              class="topic_text_TH nav_item block z-0 py-2 px-1 text-white rounded hover:bg-gray-100 hover:bg-transparent "
-              >{{ item.name }}</a
-            >
-          </li>
-        </ul>
+      <ul
+        class="hidden w-auto lg:flex items-center p-4 md:p-0 border rounded-lg md:flex-row md:space-x-8 rtl:space-x-reverse mt-0 border-0 "
+      >
+        <li v-for="(item, index) in menubar" :key="index">
+          <a
+            v-if="index == 4"
+            @click="fn_getName"
+            href="/"
+            class="topic_text_TH outline_solid py-2.5 block z-1 xl:px-5 md:px-2 text-white rounded md:text-white hover:text-white"
+            aria-current="page"
+            >{{ item.name }}</a
+          >
+          <a
+            v-else
+            @click="fn_getName"
+            :href="item.name_path"
+            class="topic_text_TH nav_item block z-0 px-2 text-white rounded hover:bg-gray-100 hover:bg-transparent "
+            >{{ item.name }}</a
+          >
+        </li>
+      </ul>
+      <div class="item_mobile lg:hidden">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="text-white w-6 h-7">
+          <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+        </svg>
       </div>
-    </div>
-  </nav>
+    </nav>
+    <!-- <nav class="nav_moblie lg:hidden flex items-center justify-between bg-black">
+      <a href="/" class=" space-x-3 rtl:space-x-reverse">
+        <span
+          class="sty_logo self-center text-lg font-semibold whitespace-nowrap text-white"
+          >Gavyn Miles</span
+        >
+      </a>
+      <div class="item_mobile">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="text-white w-6 h-7">
+          <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+        </svg>
+      </div>
+    </nav> -->
+  </section>
 </template>
 
 <script>
@@ -79,6 +92,7 @@ export default {
 <style scoped>
 .sty_logo{
   font-family: 'Lemon', serif;
+  /* font-family: 'K2D', sans-serif; */
   font-size: 1.7rem;
   font-weight: 500;
 }
@@ -90,9 +104,15 @@ export default {
 }
 .topic_text_TH {
   font-weight: 500;
-  font-size: 1.37rem;
-  display: flex;
-  justify-content: center;
+  font-size: 1.15rem;
+  font-family: 'K2D', sans-serif;
+}
+.outline_solid{
+  background: -webkit-linear-gradient(#a993fe -0.13%, #7e61e7 99.87%);
+}
+.outline_solid:hover{
+  outline: 2px solid #7e61e7;
+  background: transparent;
 }
 .nav_item{
   cursor: pointer;
